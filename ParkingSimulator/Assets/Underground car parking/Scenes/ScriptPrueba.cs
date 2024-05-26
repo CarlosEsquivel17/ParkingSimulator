6d7e55bd763d4bcf104fd2431d8ddff78dc61466
Ruben174520 <174520@upslp.edu.mx>
2024-05-25 18:47:10 -0600

    ADAPTACION VR Y CARRO MOVIMIENTO
    No mover el camara offset porque se mueve la camara principal

---
+++ ParkingSimulator/Assets/Underground car parking/Scenes/ScriptPrueba.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class ScriptPrueba : MonoBehaviour
+{
+    public float motorForce = 1500f;
+    public float brakeForce = 3000f;
+    public float maxSteerAngle = 30f;
+    public WheelCollider frontLeftWheel, frontRightWheel;
+    public WheelCollider rearLeftWheel, rearRightWheel;
+    public Transform frontLeftTransform, frontRightTransform;
+    public Transform rearLeftTransform, rearRightTransform;
+
+    private float currentSteerAngle;
+    private float currentBrakeForce;
+    private bool isBraking;
+
+    private void FixedUpdate()
+    {
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void HandleMotor()
+    {
+        float motor = motorForce * Input.GetAxis("Vertical");
+        frontLeftWheel.motorTorque = motor;
+        frontRightWheel.motorTorque = motor;
+
+        currentBrakeForce = isBraking ? brakeForce : 0f;
+        ApplyBraking();
+    }
+
+    private void ApplyBraking()
+    {
+        frontLeftWheel.brakeTorque = currentBrakeForce;
+        frontRightWheel.brakeTorque = currentBrakeForce;
+        rearLeftWheel.brakeTorque = currentBrakeForce;
+        rearRightWheel.brakeTorque = currentBrakeForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * Input.GetAxis("Horizontal");
+        frontLeftWheel.steerAngle = currentSteerAngle;
+        frontRightWheel.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheel, frontLeftTransform);
+        UpdateSingleWheel(frontRightWheel, frontRightTransform);
+        UpdateSingleWheel(rearLeftWheel, rearLeftTransform);
+        UpdateSingleWheel(rearRightWheel, rearRightTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
+    }
+}
+
